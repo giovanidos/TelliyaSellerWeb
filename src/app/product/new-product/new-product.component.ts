b84223f22a84845e6d72b2e789646cc7505b9ff4
Lassine-Konate <lassineknt@gmail.com>
2019-08-30 07:52:46 -0600

    update new product ver1

--- src/app/product/new-product/new-product.component.ts
+++ src/app/product/new-product/new-product.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-product.component.scss']
 })
 export class NewProductComponent implements OnInit {
+  Marcher:any=[
+    {id:1, name:'Consommation', categorie:['biski','Bonbon'], marque:['neant']},
+    {id:2, name:'Mode', categorie:['Veste','pujama'], marque:['neant', 'Cello', 'Cucci']},
+    {id:3, name:'Librairie', categorie:['Livre','Cahier'], marque:['neant']},
+    {id:4, name:'Informatique', categorie:['Ordinateur','USB'], marque:['neant','apple','Oppo']}
+  ];
+  table:string[];
+  valuer:number;
+  fileTOUpload: File= null;
+  imageurl:string="";
 
   constructor() { }
 
   ngOnInit() {
+    this.valuer=1;
+   
   }
-
+  ficherupload(file: FileList){
+    this.fileTOUpload= file.item(0);
+    var reader= new FileReader();
+    reader.onload= (event: any) => {
+      this.imageurl = event.target.resultat;
+    }
+    reader.readAsDataURL(this.fileTOUpload);
+  }
 }
